f2033b7c567d40d24ffe50250e9d81ba06583ed2
matheussla <matheus.la1999@gmail.com>
2025-04-07 15:19:42 -0300

    feat: adjust swagger

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+  URL: process.env.URL || 'http://localhost:3000',
   nodeEnv: process.env.NODE_ENV || 'development',
   apiPrefix: process.env.API_PREFIX || '/api/v1',
   jwtSecret: process.env.JWT_SECRET || 'your-secret-key',
